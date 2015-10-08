4171d1c9b109d5e8259f5981535665721e580523
totearppship <totearppship@gmail.com>
2015-10-08 10:02:42 +0900

    sprint boot tutorial

---
+++ src/main/java/com/ppship/spring_boot_helloworld/domain/CommonObject.java
@@ -0,0 +1,8 @@
+package com.ppship.spring_boot_helloworld.domain;
+
+public class CommonObject {
+@Override
+public String toString() {
+	return super.toString();
+}
+}
---
+++ src/test/java/com/ppship/spring_boot_helloworld/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.ppship.spring_boot_helloworld;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class SimpleTest {
+}
